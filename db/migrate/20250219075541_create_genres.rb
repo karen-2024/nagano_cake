3ffc87b5850593e9fd90a5f900972b98fa9acd18
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2025-02-19 09:01:26 +0000

    [Add]Customer・Admin以外のモデル作成

---
+++ db/migrate/20250219075541_create_genres.rb
@@ -0,0 +1,10 @@
+class CreateGenres < ActiveRecord::Migration[6.1]
+  def change
+    create_table :genres do |t|
+      t.integer :customer_id, null: false
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
